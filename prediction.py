ebdc8b568dfd04a2302720e04cc287ca82ed12fa
Lonewarrior147 <147608592+Lonewarrior147@users.noreply.github.com>
2024-04-22 21:46:24 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,4 +1,4 @@
 import joblib
 def predict(data):
-    clf = joblib.load("C:\Users\rakes\Downloads\diabetes+130-us+hospitals+for+years+1999-2008 (1)\diabetic_data.csv")
+    clf = joblib.load(r"C:\Users\rakes\Downloads\diabetes+130-us+hospitals+for+years+1999-2008 (1)\diabetic_data.csv")
     return clf.predict(data)
